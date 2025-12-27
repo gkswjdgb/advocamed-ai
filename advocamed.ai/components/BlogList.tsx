d1966c5037f9a62d61d4fd5e22ceec6ede9ae6d7
gkswjdgb <junghyuhan@gmail.com>
2025-12-27 23:10:47 +0900

    Update and rename BlogList to BlogList.tsx

--- advocamed.ai/components/BlogList.tsx
+++ advocamed.ai/components/BlogList.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BlogPost } from '../types';
-import { blogPosts } from '../data/blogPosts';
+import { blogPosts } from '../data/blogData';
 
 interface BlogListProps {
   onPostClick: (post: BlogPost) => void;
