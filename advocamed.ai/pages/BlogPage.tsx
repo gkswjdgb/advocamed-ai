396537515c2bd29c2c0bb3b84eb4cebf1d475924
gkswjdgb <junghyuhan@gmail.com>
2026-01-02 22:44:07 +0900

    Update BlogPage.tsx

--- advocamed.ai/pages/BlogPage.tsx
+++ advocamed.ai/pages/BlogPage.tsx
@@ -1,7 +1,6 @@
-
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { blogPosts } from '../data/blogData';
+import { blogPosts } from '../data/blogPosts';
 import SEO from '../components/SEO';
 
 const BlogPage: React.FC = () => {
