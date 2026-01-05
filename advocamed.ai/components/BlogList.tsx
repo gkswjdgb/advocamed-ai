d87ade3196038bc476b67d7faca145e0560eb914
gkswjdgb <junghyuhan@gmail.com>
2026-01-05 09:12:09 +0900

    Update BlogList.tsx

--- advocamed.ai/components/BlogList.tsx
+++ advocamed.ai/components/BlogList.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BlogPost } from '../types';
-import { blogPosts } from '../data/blogData';
+import { blogPosts } from '../data/blogPosts';
 
 interface BlogListProps {
   onPostClick: (post: BlogPost) => void;
